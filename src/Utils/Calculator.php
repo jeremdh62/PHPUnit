1f3d418968a622028943dc1c049890e7961062d9
jeremdh62 <jeremdh.dev@outlook.com>
2022-01-25 19:56:16 +0100

    ajout fonction mul, div et avg avec test

--- src/Utils/Calculator.php
+++ src/Utils/Calculator.php
@@ -2,15 +2,37 @@
 
 namespace App\Utils;
 
-class Calculator 
+class Calculator
 {
-    public function add(int $a,int $b) : int
+    public function add(int $a, int $b): int
     {
-        return $a + $b ;
+        return $a + $b;
     }
 
-    public function sub(int $a,int $b): int
+    public function sub(int $a, int $b): int
     {
-        return $a - $b ;
+        return $a - $b;
+    }
+
+    public function mul(int $a, int $b)
+    {
+        return $a * $b;
+    }
+
+    public function div(int $a, int $b)
+    {
+        return $a / $b;
+    }
+
+    public function avg(array $tab)
+    {
+        $total = 0;
+        foreach ($tab as $val) {
+            $total = $total + $val;
+        }
+
+        $result = $total / count($tab);
+
+        return $result;
     }
 }
--- tests/Utils/CalculatorTest.php
+++ tests/Utils/CalculatorTest.php
@@ -13,23 +13,54 @@ class CalculatorTest extends TestCase
     {
         $calculator = new Calculator();
 
-        $result = $calculator->add(4,8);
+        $result = $calculator->add(4, 8);
 
         //assertEquals permet de comparer 2 valeurs
         //$this->assertEquals(12,$result);
 
         /*assertSame permet de comparer 2 valeurs mais aussi 
         de voir si le type est le même */
-        $this->assertSame(12,$result);
+        $this->assertSame(12, $result);
         //$this->assertSame(12,"12");
     }
 
     /** @test */
-    public function can_substract_two_integers() {
+    public function can_substract_two_integers()
+    {
         $calculator = new Calculator();
 
-        $result = $calculator->sub(4,8);
+        $result = $calculator->sub(4, 8);
 
-        $this->assertSame(-4,$result);
+        $this->assertSame(-4, $result);
+    }
+
+    /** @test */
+    public function can_multiply_two_integers()
+    {
+        $calculator = new Calculator();
+
+        $result = $calculator->mul(2, 4);
+
+        $this->assertSame(8, $result);
+    }
+
+    /** @test */
+    public function can_divise_two_integers()
+    {
+        $calculator = new Calculator();
+
+        $result = $calculator->div(4, 2);
+
+        $this->assertSame(2, $result);
+    }
+
+    /** @test */
+    public function can_avg_two_integers()
+    {
+        $calculator = new Calculator();
+
+        $result = $calculator->avg([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+        $this->assertSame(5, $result);
     }
 }
